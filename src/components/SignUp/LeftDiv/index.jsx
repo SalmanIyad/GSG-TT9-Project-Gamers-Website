8f12696ef4d0dce448c7a997813ef3f1a297940a
Salman <salman@kali.Salman>
2023-08-14 22:25:37 +0300

    v2 - dev branch

--- src/components/SignUp/LeftDiv/index.jsx
+++ src/components/SignUp/LeftDiv/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import * as yup from "yup";
 import styles from "./style.module.css";
 import Container from "../../Container";
@@ -62,7 +62,6 @@ const schema = yup.object().shape({
     .required("Repeat password is required")
     .oneOf([yup.ref("password"), null], "Passwords must match"),
   terms: yup.boolean().oneOf([true], 'You must agree to the terms and conditions'),
-  
 });
 
 const LeftDiv = () => {
@@ -74,7 +73,12 @@ const LeftDiv = () => {
     resolver: yupResolver(schema),
   });
   const { signup, isLoading } = useAuthContext();
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
   const onHandleSubmit = (body) => {
     const data = {
@@ -84,6 +88,8 @@ const LeftDiv = () => {
     };
     signup(data);
   };
+
+
   return (
     <div className={styles.section}>
       <div
@@ -114,15 +120,30 @@ const LeftDiv = () => {
                   {...register(input.name)}
                   id={input.id}
                   name={input.name}
-                  type={input.type}
+                  type={showPassword ? "text" : input.type}
                   className={styles.form__input}
                   placeholder={input.placeholder}
-                />
-                {errors[input.name] && (
-                  <span className={styles.errors}>
-                    {errors[input.name].message}
-                  </span>
-                )}
+                />  
+                {input.name === 'password' ? 
+                  (showPassword ? (
+                    <svg
+                      onClick={handleShowPassword}
+                      stroke='#8692A6'
+                      fill='#8692A6'
+                      strokeWidth='0'
+                      viewBox='0 0 24 24'
+                      height='2rem'
+                      width='2rem'
+                      xmlns='http://www.w3.org/2000/svg'>
+                      <path d='M12 19c.946 0 1.81-.103 2.598-.281l-1.757-1.757c-.273.021-.55.038-.841.038-5.351 0-7.424-3.846-7.926-5a8.642 8.642 0 0 1 1.508-2.297L4.184 8.305c-1.538 1.667-2.121 3.346-2.132 3.379a.994.994 0 0 0 0 .633C2.073 12.383 4.367 19 12 19zm0-14c-1.837 0-3.346.396-4.604.981L3.707 2.293 2.293 3.707l18 18 1.414-1.414-3.319-3.319c2.614-1.951 3.547-4.615 3.561-4.657a.994.994 0 0 0 0-.633C21.927 11.617 19.633 5 12 5zm4.972 10.558-2.28-2.28c.19-.39.308-.819.308-1.278 0-1.641-1.359-3-3-3-.459 0-.888.118-1.277.309L8.915 7.501A9.26 9.26 0 0 1 12 7c5.351 0 7.424 3.846 7.926 5-.302.692-1.166 2.342-2.954 3.558z'></path>
+                    </svg>
+                  ) : (
+                    <Image
+                      handleShowPassword={handleShowPassword}
+                      ImageSrc='/assets/Vector.svg'
+                    />
+                  ))
+                : null }
               </div>
             ))}
             <div className={styles.form_checkbox}>
--- src/constants/TableColumns.js
+++ src/constants/TableColumns.js
@@ -12,11 +12,22 @@ export const TABLE_COLUMNS = (handleDelete, handleEdit) => [
     title: 'Email',
   },
   {
+    key: 'isAdmin',
+    title: 'Type',
+    render: (data) => (
+      data.isAdmin ? 'Admin' : 'User'
+    ),
+  },
+  {
+    key: 'createdAt',
+    title: 'Create time',
+  },
+  { 
     key: 'actions',
     title: 'Actions',
     render: (data) => (
       <div onClick={(e) => e.stopPropagation()}>
-        <button onClick={() => handleDelete(data._id)}>delete</button>
+        <button onClick={() => handleDelete(data._id)}>Delete</button>
       </div>
     ),
   },
